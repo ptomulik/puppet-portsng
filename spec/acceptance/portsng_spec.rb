970ade5e4ace5e9c0e68f1e1414df9d6ba5ea7cc
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2016-09-10 22:21:25 +0200

    added acceptance tests for package_settings

--- spec/acceptance/portsng_spec.rb
+++ spec/acceptance/portsng_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper_acceptance'
 
 describe 'portsng provider' do
 
+  def package_setting(portname, optname)
+    cmd = "make -C /usr/ports/#{portname} showconfig | awk -F '[=:]' '/#{optname}/ {print $2}'"
+    shell(cmd).stdout.strip
+  end
+
   skip_tests = true
   case fact('osfamily')
   when 'FreeBSD'
-    portname = 'textproc/jq'
+    portname = 'misc/ddate'
     skip_tests = false
   end
 
@@ -41,11 +46,11 @@ describe 'portsng provider' do
     end
   end
 
-  context "package {'#{portname}': package_settings => {'DOCS' => false} }" do
+  context "package {'#{portname}': package_settings => {'USFORMAT' => false} }" do
     it 'runs without an error' do
       pp = <<-PP
         Package{ provider => portsng }
-        package{ '#{portname}': ensure => present, package_settings => {'DOCS' => false} }
+        package{ '#{portname}': ensure => present, package_settings => {'USFORMAT' => false} }
       PP
       # Run it twice for idempotency
       apply_manifest(pp, :catch_failures => true)
@@ -54,14 +59,23 @@ describe 'portsng provider' do
 
     describe package(portname) do
       it { is_expected.to be_installed }
+    end
+    describe "package option USFORMAT" do
+      it { expect(package_setting(portname, 'USFORMAT')).to eq "off" }
+    end
+    describe "package option KILLBOB" do
+      it { expect(package_setting(portname, 'KILLBOB')).to eq "on" }
+    end
+    describe "package option PRAISEBOB" do
+      it { expect(package_setting(portname, 'PRAISEBOB')).to eq "off" }
     end
   end
 
-  context "package {'textproc/jq': package_settings => {'DOCS' => true}}" do
+  context "package {'#{portname}': package_settings => {'USFORMAT' => true}}" do
     it 'runs without an error' do
       pp = <<-PP
         Package{ provider => portsng }
-        package{ '#{portname}': ensure => present, package_settings => {'DOCS' => true} }
+        package{ '#{portname}': ensure => present, package_settings => {'USFORMAT' => true} }
       PP
       # Run it twice for idempotency
       apply_manifest(pp, :catch_failures => true)
@@ -70,6 +84,15 @@ describe 'portsng provider' do
 
     describe package(portname) do
       it { is_expected.to be_installed }
+    end
+    describe "package option USFORMAT" do
+      it { expect(package_setting(portname, 'USFORMAT')).to eq "on" }
+    end
+    describe "package option KILLBOB" do
+      it { expect(package_setting(portname, 'KILLBOB')).to eq "on" }
+    end
+    describe "package option PRAISEBOB" do
+      it { expect(package_setting(portname, 'PRAISEBOB')).to eq "off" }
     end
   end
 end
