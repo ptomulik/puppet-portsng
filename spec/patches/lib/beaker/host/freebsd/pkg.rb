133b0f00d17d7c023050b029a5a5237fd4e74619
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2016-09-21 00:35:40 +0200

    revise the beaker patch

--- spec/patches/lib/beaker/host/freebsd/pkg.rb
+++ spec/patches/lib/beaker/host/freebsd/pkg.rb
@@ -2,27 +2,38 @@
 module FreeBSD::Pkg
   include Beaker::CommandFactory
 
-  def shell_check_pkgng
+  def pkg_info_pattern(package)
+    # This seemingly restrictive pattern prevents false positives...
+    "^#{package}-[0-9][0-9a-zA-Z_\\.,]*$"
+  end
+
+  def check_pkgng_sh
     'TMPDIR=/dev/null ASSUME_ALWAYS_YES=1 PACKAGESITE=file:///nonexist ' \
     'pkg info -x "pkg(-devel)?\\$" > /dev/null 2>&1'
   end
 
-  def shell_ifelse(cond, stmt_t, stmt_f)
-    "if #{cond}; then #{stmt_t}; else #{stmt_f}; fi"
+  def pkgng_active?(opts = {})
+    opts = {:accept_all_exit_codes => true}.merge(opts)
+    execute("/bin/sh -c '#{check_pkgng_sh}'", opts) { |r| r }.exit_code == 0
   end
 
   def install_package(package, cmdline_args = nil, opts = {})
-    cmd = shell_ifelse shell_check_pkgng,
-                       "pkg install #{cmdline_args || '-y'} #{package}",
-                       "pkg_add #{cmdline_args || '-r'} #{package}"
-    execute("/bin/sh -c '#{cmd}'", opts) { |result| result }
+    cmd = if pkgng_active?
+            "pkg install #{cmdline_args || '-y'} #{package}"
+          else
+            "pkg_add #{cmdline_args || '-r'} #{package}"
+          end
+    execute(cmd, opts) { |result| result }
   end
 
   def check_for_package(package, opts = {})
-    cmd = shell_ifelse shell_check_pkgng,
-                       "pkg info #{package}",
-                       "pkg_info -Iqx #{package} 2> /dev/null || true"
-    execute("/bin/sh -c '#{cmd}'", opts) { |result| result }
+    opts = {:accept_all_exit_codes => true}.merge(opts)
+    cmd = if pkgng_active?
+            "pkg info #{package}"
+          else
+            "pkg_info -Ix '#{pkg_info_pattern(package)}'"
+          end
+    execute(cmd, opts) { |result| result }.exit_code == 0
   end
 end
 # rubocop: enable ClassAndModuleChildren
